67d76fd09cce9e0ccded2918e763a528fdf31abf
teoCardoso7 <teo.cardoso.7@gmail.com>
2019-02-27 18:01:03 -0300

    Backend Completed

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import {TodoService } from '../../services/todo.service'
+import { elementEnd } from '@angular/core/src/render3';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  todoListArray: any[];
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-  }
+    this.todoService.getTodoList().snapshotChanges()
+    .subscribe(item => {
+      this.todoListArray = [];
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x['$key'] = element.key;
+        this.todoListArray.push(x);
 
+      });
+      
+      this.todoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+
+      })
+    })
+  }
+  addToDo(itemTitle){
+    this.todoService.addTodo(itemTitle.value);
+    itemTitle.value = null;
+    
+  }
+  updateTodo($key:string, isChecked:boolean){
+    this.todoService.udpdateTodo($key, !isChecked)
+  }
+  removeTodo($key){
+    if(confirm("Are you sure?")){
+    this.todoService.deleteTodo($key);
+    }
+    
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database'
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todoList: AngularFireList<any>;
+  constructor(private firebasedb: AngularFireDatabase) { }
+  getTodoList(){
+this.todoList = this.firebasedb.list('titles')
+return this.todoList;
+  }
 
-  constructor() { }
+  addTodo(title: string){
+this.todoList.push({
+  title:title,
+  isChecked: false
+})
+  }
+  udpdateTodo($key: string, flag: boolean){
+this.todoList.update($key, {isChecked: flag})
+  }
+  deleteTodo($key){
+    this.todoList.remove($key)
+  }
 }
