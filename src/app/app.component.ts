52ec43762a9b0deed1221a078cb0ca70704062e0
teoCardoso7 <teo.cardoso.7@gmail.com>
2019-02-27 01:37:18 -0300

    Front end part2
    Here I start to display class items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {Tasks} from './models/tasks'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-toDoList';
+  taskArray: Tasks[] = [
+    {id:232, title:"First task"},
+    {id:233, title:"Second task"},
+    {id:234, title:"Third task"}
+  ];
+
+  
 }
